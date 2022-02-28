cdb668f98a6e650cb779698221a7f01f9bfecfb1
Abhijeet Shyam Jagtap <aj.abhijeet21@hotmail.com>
2022-02-28 12:26:27 +0530

    Cleaned main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:superprocure_app/screens/dashboard.dart';
-
-import 'package:superprocure_app/screens/location_tracking.dart';
 
 void main() {
   if (defaultTargetPlatform == TargetPlatform.android) {
@@ -24,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DashBoard(),
+      home: const DashBoard(),
     );
   }
 }
